75a9405574018e50a005d8d1b0d40b17bc9e7dd2
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-04-02 14:22:34 +1200

    Improved test coverage

--- tests/specs/backbone-shortcuts_test.js
+++ tests/specs/backbone-shortcuts_test.js
@@ -1,15 +1,74 @@
 define([
+  'underscore',
   'backbone',
   'backbone-shortcuts'
-], function(Backbone){
+], function(_, Backbone){
+  'use strict';
 
-  describe("When a new view is created", function () {
+  var spies = {};
 
-    it("should registered the shortcuts", function () {
-      Backbone.Mediator.publish = sinon.stub();
-      var view = new Backbone.View();
-      view.setup();
-      expect(Backbone.Mediator.publish).toHaveBeenCalledWith('shortcuts:add', view.shortcuts, view);
+  describe("Backbone-Shortcuts", function () {
+
+    beforeEach(function(){
+      $('body').append('<div id="container"><a></a></div>');
+    });
+    afterEach(function(){
+      _.each(spies, function(spy){
+        spy.restore();
+      });
+    });
+
+
+    describe("When a new view is created", function () {
+
+      var view;
+      beforeEach(function(){
+
+        spies.delegateShortcuts = sinon.spy(Backbone.Shortcuts, 'delegateShortcuts');
+        var View = Backbone.View.extend({
+          el: '#container',
+          shortcuts: {
+            'enter': 'my_function'
+          }
+        });
+        view = new View();
+      });
+
+      afterEach(function(){
+        view.remove();
+      });
+
+      describe("And the view has a 'shortcuts' object", function () {
+
+        it("should delegate the shortcuts", function () {
+          expect(spies.delegateShortcuts).toHaveBeenCalledWith(view.shortcuts);
+        });
+
+      });
+
+    });
+
+    describe("When the view's events are undelegated", function () {
+
+      var view;
+      beforeEach(function(){
+
+        spies.undelegateShortcuts = sinon.spy(Backbone.Shortcuts, 'undelegateShortcuts');
+        var View = Backbone.View.extend({
+          el: '#container',
+          shortcuts: {
+            'enter': 'my_function'
+          }
+        });
+        view = new View();
+      });
+
+      it("Should undelegate the shortcuts", function () {
+        view.undelegateEvents();
+        expect(spies.undelegateShortcuts).toHaveBeenCalled(view.shortcuts);
+      });
+
+
     });
 
   });
